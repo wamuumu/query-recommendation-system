f8a32e86d911e11eb023617fdb311934d20c00b1
Matteo <matteo68.mc@gmail.com>
2022-11-23 19:25:14 +0100

    Added data reading and recommender class

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 import pandas as pd
+import pandasql as ps
+
+class Recommender:
+
+	def __init__(self, dataset):
+		self.dataset = dataset
+
+	def compute_query(self):
+		df = self.dataset
+		return ps.sqldf("select * from df where id = 1") 
+
 
 if __name__ == "__main__":
-	pass
+		
+	users = pd.read_csv("./resources/output/users.csv", names = ["id"], header = None)
+	dataset = pd.read_csv("./resources/output/dataset.csv", names = ["id","name","address","age","occupation"], header = 0)
+	ratings = pd.read_csv("./resources/output/utility_matrix.csv")
+
+	queriesIDs = list(ratings.columns)
+	usersIDs = list(ratings.index.values)
+
+	'''print(users)
+	print(dataset)
+	print(ratings)
+
+	print(queriesIDs)
+	print(usersIDs)'''
+
+	recommender = Recommender(dataset)
+	result = recommender.compute_query() #lista campi in conjunction
+
+	print(result)
+
+	exit(0)
